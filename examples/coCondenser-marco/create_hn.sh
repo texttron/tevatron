d1d1c9547572b0a4a73bb1ec6a441ee0bb58aa87
luyug <luyug@andrew.cmu.edu>
2021-09-08 10:13:00 -0400

    [coCondenser] Add Marco hard negative script

---
+++ examples/coCondenser-marco/create_hn.sh
@@ -0,0 +1,6 @@
+SCRIPT_DIR=$PWD/../msmarco-passage-ranking
+cd marco
+python $SCRIPT_DIR/build_hn.py --tokenizer_name bert-base-uncased --hn_file ../train.rank.txt --qrels qrels.train.tsv \
+  --queries train.query.txt --collection corpus.tsv --save_to bert/train-hn
+ln -s bert/train/* bert/train-hn
+cd -
