3531fb5ebee83d393f2faad848179b90dc983a72
luyug <luyug@andrew.cmu.edu>
2021-12-08 15:19:15 -0800

    add dpr conversion; fix bugs

--- examples/coCondenser-nq/prepare_wiki_train.py
+++ examples/coCondenser-nq/prepare_wiki_train.py
@@ -9,7 +9,7 @@ parser = ArgumentParser()
 parser.add_argument('--input', type=str, required=True)
 parser.add_argument('--output', type=str, required=True)
 parser.add_argument('--tokenizer', type=str, required=False, default='bert-base-uncased')
-parser.add_argument('--minimum-negatives', type=int, required=False, default=8)
+parser.add_argument('--minimum-negatives', type=int, required=False, default=1)
 args = parser.parse_args()
 
 tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(args.tokenizer, use_fast=True)
@@ -20,12 +20,12 @@ if not os.path.exists(args.output):
     os.makedirs(args.output)
 with open(os.path.join(args.output, 'train_data.json'), 'w') as f:
     for idx, item in enumerate(tqdm(data)):
-        if len(item['hard_negative_ctxs']) < args.minimum_negatives and len(item['positive_ctxs']) < 1:
+        if len(item['hard_negative_ctxs']) < args.minimum_negatives or len(item['positive_ctxs']) < 1:
             continue
 
         group = {}
-        positives = [pos['title'] + tokenizer.pad_token + pos['text'] for pos in item['positive_ctxs']]
-        negatives = [neg['title'] + tokenizer.pad_token + neg['text'] for neg in item['hard_negative_ctxs']]
+        positives = [pos['title'] + tokenizer.sep_token + pos['text'] for pos in item['positive_ctxs']]
+        negatives = [neg['title'] + tokenizer.sep_token + neg['text'] for neg in item['hard_negative_ctxs']]
 
         query = tokenizer.encode(item['question'], add_special_tokens=False, max_length=256, truncation=True)
         positives = tokenizer(
