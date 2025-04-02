33a17bd8f06ff455dae79f0b51415872b92c6f96
ArvinZhuang <shengyaozhuang@gmail.com>
2025-04-02 20:56:35 +1000

    update splade example

---
+++ examples/splade/convert_result_to_trec.py
@@ -0,0 +1,20 @@
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('--input', type=str, required=True)
+parser.add_argument('--output', type=str, required=True)
+parser.add_argument('--remove_query', action='store_true')
+args = parser.parse_args()
+
+with open(args.input) as f_in, open(args.output, 'w') as f_out:
+    cur_qid = None
+    rank = 0
+    for line in f_in:
+        qid, docid, score = line.split()
+        if cur_qid != qid:
+            cur_qid = qid
+            rank = 0
+        if args.remove_query and qid == docid:
+            continue
+        rank += 1      
+        f_out.write(f'{qid} Q0 {docid} {rank} {-rank} dense\n')
